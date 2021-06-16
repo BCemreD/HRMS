478d4279c2817d985adc63bc0429d67a6aad8aa3
BCemreD <72667928+BCemreD@users.noreply.github.com>
2021-06-16 20:11:57 +0300

    mernis hatası giderildi

--- src/main/java/hrms/hrms/entities/concretes/Employer.java
+++ src/main/java/hrms/hrms/entities/concretes/Employer.java
@@ -4,9 +4,8 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
-import lombok.AllArgsConstructor;
+
 import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
 
 
 @EqualsAndHashCode(callSuper = false)
